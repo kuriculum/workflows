180b745a1734f92c4ea46b40be1919397334d413
ruby -v <adiben.baron@iamplus.com>
2016-09-25 15:52:32 +0300

    Concat javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,23 @@
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var concat = require('gulp-concat');
+
+var jsSources = ['components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+];
+
 gulp.task('coffee', function () {
     gulp.src(coffeeSources)
         .pipe(coffee({ bare: true })
         .on('error', gutil.log))
         .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function () {
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
 });
