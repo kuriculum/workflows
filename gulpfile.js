37e7f077e39f654b527faf093b6cc1728552e4ec
ruby -v <adiben.baron@iamplus.com>
2016-09-25 15:40:13 +0300

    add coffee files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 ﻿var gulp = require('gulp');
 var gutil = require('gulp-util');
-
-gulp.task('log', function () {
-    gutil.log('workflows is shit!!')
+var coffee = require('gulp-coffee');
+var coffeeSources = ['components/coffee/tagline.coffee'];
+gulp.task('coffee', function () {
+    gulp.src(coffeeSources)
+        .pipe(coffee({ bare: true })
+        .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'))
 });
